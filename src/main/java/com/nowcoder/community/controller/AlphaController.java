a1133c7d442bd72a0e6d56c97c4c339661dbf792
hqc <1697093422@qq.com>
2022-03-04 23:29:48 +0800

    敏感词过滤

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,5 +1,6 @@
 package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -41,7 +44,7 @@ public class AlphaController {
         response.setContentType("text/html;charset=utf-8");
         try {
             PrintWriter writer = response.getWriter();
-            writer.write("<h1>凝光</h1>");
+            writer.write("<h1>cb</h1>");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -98,5 +101,37 @@ public class AlphaController {
         emp.put("age",40);
         emp.put("salary",100000);
         return emp;
+    }
+
+    @RequestMapping(path = "/cookie/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        cookie.setPath("/community/alpha");
+        cookie.setMaxAge(60*10);
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code")String code){
+        return code;
+    }
+
+    @RequestMapping(path = "/session/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session){
+        session.setAttribute("id",1);
+        session.setAttribute("name","TestSession");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session){
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
     }
 }
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,20 +26,23 @@ public class MapperTests {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
     @Test
-    public void testSelectUser(){
-        User user=userMapper.selectById(101);
+    public void testSelectUser() {
+        User user = userMapper.selectById(101);
         System.out.println(user);
 
-        user=userMapper.selectByName("liubei");
+        user = userMapper.selectByName("liubei");
         System.out.println(user);
 
         System.out.println(userMapper.selectByEmail("nowcoder101@sina.com"));
     }
 
     @Test
-    public void testInsertUser(){
-        User user=new User();
+    public void testInsertUser() {
+        User user = new User();
         user.setUsername("test");
         user.setPassword("123456");
         user.setSalt("abc");
@@ -45,31 +50,53 @@ public class MapperTests {
         user.setHeaderUrl("http://www.nowcoder.com/101.png");
         user.setCreateTime(new Date());
 
-        int rows=userMapper.insertUser(user);
+        int rows = userMapper.insertUser(user);
         System.out.println(rows);
         System.out.println(user.getId());
 
     }
 
     @Test
-    public void testUpdateUser(){
-        int rows=userMapper.updateStatus(150,1);
+    public void testUpdateUser() {
+        int rows = userMapper.updateStatus(150, 1);
         System.out.println(rows);
 
-        rows=userMapper.updateHeader(150,"http://www.nowcoder.com/102.png");
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
         System.out.println(rows);
 
-        rows=userMapper.updatePassword(150,"hello");
+        rows = userMapper.updatePassword(150, "hello");
         System.out.println(rows);
     }
 
     @Test
-    public void testSelectPosts(){
+    public void testSelectPosts() {
         List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(149, 0, 10);
-        for(DiscussPost post:discussPosts){
+        for (DiscussPost post : discussPosts) {
             System.out.println(post);
         }
-        int rows=discussPostMapper.selectDiscussPostRows(149);
+        int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
     }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
+
+
 }
